59386327b14d3605d3970446820a279646e0f807
rawfishx <ahmadtarbiji@gmail.com>
2025-06-25 10:10:02 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@
 # Pastikan folder bin ada
 
 # Pindah file satria ke ~/bin
-cp satria ../usr/bin/satria
+cp satria /data/data/com.termux/files/usr/bin/
 
 # Kasih izin eksekusi
-chmod +x ../usr/bin/satria
+chmod +x /data/data/com.termux/files/usr/bin/
 
 # Tambahkan ke PATH di .bashrc (kalau belum ada)
 if ! grep -q 'export PATH="$HOME/bin:$PATH"' ~/.bashrc; then
